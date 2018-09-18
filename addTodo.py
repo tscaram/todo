c84e421bc64ec2c17c6c1d17940a0157f1ae47d5
Tyler <tscaram@gmail.com>
2018-09-17 22:17:40 -0400

    First commit

---
+++ addTodo.py
@@ -0,0 +1,11 @@
+path = "/home/tyler/Desktop/Todo/todo.txt"
+
+def main():
+
+	print("What would you like to add to do? ")
+	item = raw_input()
+	theFile = open(path, 'a')
+	theFile.write(item + '\n')
+
+
+main()
---
+++ todo.py
@@ -0,0 +1,42 @@
+
+path = "/home/tyler/Desktop/Todo/todo.txt"
+finishedPath = "/home/tyler/Desktop/Todo/finished.txt"
+
+def main():
+	print("Here is the list of things to do today: ")
+
+	theFile = open(path, 'r')
+	items = theFile.read().splitlines()
+
+	for i in range(0, len(items)):
+		print(str(i) + ". " + items[i])
+
+	while len(items) != 0:
+		theInput = raw_input()
+		
+		if theInput == "quit":
+			theFile.close()
+			quit()
+
+		for i in range(0, len(items)):
+			if items[i] == str(theInput):
+				finishedFile = open(finishedPath, 'a')
+				finishedFile.write(items[i] + '\n')
+				finishedFile.close()
+				items.remove(items[i])
+				break
+		
+		newFile = open(path, 'w')
+		
+		for i in range(0, len(items)):
+			newFile.write(items[i] + '\n')
+
+		newFile.close()
+		
+		if len(items) != 0:
+			print("\nWhat else?")
+		
+		for i in range(0, len(items)):
+			print(str(i) + ". " + items[i])
+
+main()
